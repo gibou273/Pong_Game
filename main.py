0437d6845662ec55e5910e68be6da869afc73e2f
gibou273 <101921033+gibou273@users.noreply.github.com>
2024-03-23 19:08:28 +0100

    Detect collision with both right and left paddles.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ while is_playing:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
     # Detect collision with the right paddles
-    if ball.distance(right_paddle) < 50 and ball.xcor() > 340:
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
 
